46e8e38806f2213930937cc63ea5bc8c948b99a3
Joe Leonard <joeleonard@gmail.com>
2016-11-02 12:53:26 -0300

    Fix potential null crash on currentActivitiy. Revert exposing public vars for testing.

--- android/src/main/java/com/remobile/splashscreen/RCTSplashScreen.java
+++ android/src/main/java/com/remobile/splashscreen/RCTSplashScreen.java
@@ -22,9 +22,9 @@ import com.facebook.react.bridge.ReactMethod;
 
 
 public class RCTSplashScreen extends ReactContextBaseJavaModule implements LifecycleEventListener {
-    public static Dialog splashDialog;
+    private Dialog splashDialog;
     private ImageView splashImageView;
-    public static boolean splashed = false;
+    private boolean splashed = false;
 
     public RCTSplashScreen(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -63,31 +63,33 @@ public class RCTSplashScreen extends ReactContextBaseJavaModule implements Lifec
 
     private void removeSplashScreen() {
         Activity currentActivity = getCurrentActivity();
-        currentActivity.runOnUiThread(new Runnable() {
-            public void run() {
-                if (splashDialog != null && splashDialog.isShowing()) {
-                    AlphaAnimation fadeOut = new AlphaAnimation(1, 0);
-                    fadeOut.setDuration(1000);
-                    View view = ((ViewGroup)splashDialog.getWindow().getDecorView()).getChildAt(0);
-                    view.startAnimation(fadeOut);
-                    fadeOut.setAnimationListener(new Animation.AnimationListener() {
-                        @Override
-                        public void onAnimationStart(Animation animation) {
-                        }
-                        @Override
-                        public void onAnimationEnd(Animation animation) {
-                            splashDialog.dismiss();
-                            splashDialog = null;
-                            splashImageView = null;
-                            return;
-                        }
-                        @Override
-                        public void onAnimationRepeat(Animation animation) {
-                        }
-                    });
+        if (currentActivity != null) {
+            currentActivity.runOnUiThread(new Runnable() {
+                public void run() {
+                    if (splashDialog != null && splashDialog.isShowing()) {
+                        AlphaAnimation fadeOut = new AlphaAnimation(1, 0);
+                        fadeOut.setDuration(1000);
+                        View view = ((ViewGroup)splashDialog.getWindow().getDecorView()).getChildAt(0);
+                        view.startAnimation(fadeOut);
+                        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+                            @Override
+                            public void onAnimationStart(Animation animation) { }
+
+                            @Override
+                            public void onAnimationEnd(Animation animation) {
+                                splashDialog.dismiss();
+                                splashDialog = null;
+                                splashImageView = null;
+                                return;
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animation animation) { }
+                        });
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 
     private int getSplashId() {
@@ -106,36 +108,36 @@ public class RCTSplashScreen extends ReactContextBaseJavaModule implements Lifec
             return;
         }
         final Activity currentActivity = getCurrentActivity();
-        currentActivity.runOnUiThread(new Runnable() {
-            public void run() {
-                // Get reference to display
-                Display display = currentActivity.getWindowManager().getDefaultDisplay();
-                Context context = currentActivity;
+        if (currentActivity != null) {
+            currentActivity.runOnUiThread(new Runnable() {
+                public void run() {
+                    // Get reference to display
+                    Display display = currentActivity.getWindowManager().getDefaultDisplay();
+                    Context context = currentActivity;
 
-                // Use an ImageView to render the image because of its flexible scaling options.
-                splashImageView = new ImageView(context);
-                splashImageView.setImageResource(drawableId);
-                LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-                splashImageView.setLayoutParams(layoutParams);
-                splashImageView.setMinimumHeight(display.getHeight());
-                splashImageView.setMinimumWidth(display.getWidth());
-                splashImageView.setBackgroundColor(Color.BLACK);
-                splashImageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                    // Use an ImageView to render the image because of its flexible scaling options.
+                    splashImageView = new ImageView(context);
+                    splashImageView.setImageResource(drawableId);
+                    LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+                    splashImageView.setLayoutParams(layoutParams);
+                    splashImageView.setMinimumHeight(display.getHeight());
+                    splashImageView.setMinimumWidth(display.getWidth());
+                    splashImageView.setBackgroundColor(Color.BLACK);
+                    splashImageView.setScaleType(ImageView.ScaleType.FIT_XY);
 
-                // Create and show the dialog
-                splashDialog = new Dialog(context, android.R.style.Theme_Translucent_NoTitleBar);
-                // check to see if the splash screen should be full screen
-                if ((currentActivity.getWindow().getAttributes().flags & WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                        == WindowManager.LayoutParams.FLAG_FULLSCREEN) {
-                    splashDialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                    // Create and show the dialog
+                    splashDialog = new Dialog(context, android.R.style.Theme_Translucent_NoTitleBar);
+                    // check to see if the splash screen should be full screen
+                    if ((currentActivity.getWindow().getAttributes().flags & WindowManager.LayoutParams.FLAG_FULLSCREEN) == WindowManager.LayoutParams.FLAG_FULLSCREEN) {
+                        splashDialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                    }
+                    splashDialog.setContentView(splashImageView);
+                    splashDialog.setCancelable(false);
+                    if (!currentActivity.isFinishing()) {
+                        splashDialog.show();
+                    }
                 }
-                splashDialog.setContentView(splashImageView);
-                splashDialog.setCancelable(false);
-                if (!currentActivity.isFinishing()) {
-                    splashDialog.show();
-                }
-            }
-        });
+            });
+        }
     }
 }
